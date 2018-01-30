99637282391aefac79a67a9dd6c13d93cb67ad3a
Tom Woodling <t.woodling@coolblue.nl>
2018-01-30 16:44:14 +0100

    module tweak

--- Modules/crobothelpers/crobothelpers.psm1
+++ Modules/crobothelpers/crobothelpers.psm1
@@ -63,7 +63,7 @@ $json_data = @{
 
 $postParams =  @{json_data=$json_data} # Test connection
 
-$url = "https://westus.api.cognitive.microsoft.com/vision/v1.0?visualFeatures=Description&language=en"
+$url = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description&language=en"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
